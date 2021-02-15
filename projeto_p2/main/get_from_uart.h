453e7582cea110cc59140251d78d621e70705360
Luigi Luz <luigiluz98@gmail.com>
2021-02-15 13:43:18 -0500

    get_from_uart: Remove unnecessary comments

--- projeto_p2/main/get_from_uart.h
+++ projeto_p2/main/get_from_uart.h
@@ -1,10 +1,6 @@
 #ifndef GEY_FROM_UART_H_
 #define GEY_FROM_UART_H_
 
-// extern void get_from_uart_init_queue(void);
-
-// extern BaseType_t get_from_uart_read_queue(display_msg *received_msg);
-
 extern void get_from_uart_task(void *pvParameters);
 
 #endif
